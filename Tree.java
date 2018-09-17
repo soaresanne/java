175fe82c7ba1557977ba3d1187eaa940e47a3c06
Anne Soares <anne.soares@univille.br>
2018-09-17 20:18:57 -0300

    Tree

---
+++ Tree.java
@@ -0,0 +1,51 @@
+public class Tree<T> {
+	private Node<T> Root;
+
+	public Node<T> getRoot() {
+		return Root;
+	}
+
+	public void setRoot(Node<T> root) { //alterar os metodos gothrought para passar o lambda
+		Root = root;
+	}
+	
+	public void walkInTree(Node<T> node) {
+		System.out.println(node.getValue());
+		
+		for(Node<T> children: node.getChildren()) {
+			this.walkInTree(children);
+		}
+	}
+	
+	
+	//Atividade: Alterar os metodos gothrought para passar o lambda
+	
+	public void gothrough(Node<T> node) {
+		System.out.println(node.getValue());
+		
+		for(Node<T> children: node.getChildren()) {
+			this.gothrough(children);
+		}
+	}
+	
+	public void gothrough() {
+		this.gothrough(this.Root);
+	}
+		
+	
+	/*
+	adicionar
+	remover
+	percorrer
+	busca em largura: quantos nós existem?
+	busca profundidade: nó folha com maior distância do nó raiz
+	profundidade x largura: árvores com pouca hierarquia podem se tornar listas, com um nível de largura e muita profundidade.
+	Números baixos de comparações por causa das podas
+	*/
+	
+	//busca em largura (BFS)
+	//começa pela raiz e percorre seus filhos até não tiver mais filhos
+	
+	//busca em profundidade (DFS)
+	
+}
